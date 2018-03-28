ab14175b51bed9e403aa23983d68cab4dcf54b09
Sandra Połujańska <sandra.polujanska@gmail.com>
2018-03-28 09:03:45 +0200

    Add files

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+// scripts.js
+
+var url = 'https://restcountries.eu/rest/v1/name/';
+var capital = 'https://restcountries.eu/rest/v2/capital/{capital}';
+var countriesList = $('#countries');
+var capitalsList = $('#capitals');
+
+$('#search').click(searchCountries);
+
+
+function searchCountries() {
+    var countryName = $('#country-name').val();
+    if(!countryName.length) countryName = 'Poland';
+    $.ajax({
+  		url: url + countryName,
+  		method: 'GET',
+  		success: showCountriesList
+  	});
+}
+
+function showCountriesList(resp) {
+  countriesList.empty();
+    resp.forEach(function(item){
+   	$('<li>').text(item.name).appendTo(countriesList);
+   	$('<li>').text(item.capital).appendTo(capitalsList);
+    });
+}
+
+
+
+
+
+
+
+
+
